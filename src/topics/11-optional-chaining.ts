81ed5035d9b8df2b02e8444b0970f40f78d2b27c
jheisson rojas <jfrojas592@misena.edu.co>
2024-08-12 09:56:00 -0500

    final

--- src/topics/11-optional-chaining.ts
+++ src/topics/11-optional-chaining.ts
@@ -14,14 +14,14 @@ const passenger2: Passenger = {
 
 const returnChildrenNumber = (passenger: Passenger): number => { 
     
-    if ( !passenger.children ) return 0;
-    
+    // if ( !passenger.children ) return 0;
+
     // const howManyChildren = passenger.children?.length || 0;
-    const howManyChildren = passenger.children!.length;
+    const howManyChildren = passenger.children?.length || 0;
     
     console.log(passenger.name, howManyChildren);
     
+    return howManyChildren;
 }
 
-returnChildrenNumber();
-
+returnChildrenNumber (passenger1);
